9ef345a5902a7fbc62cf86a1a14b24fb4399bd89
Jean-Baptiste Vilain <jbvilain@gmail.com>
2016-11-21 23:48:52 +0100

    added missing id field in details api

--- app/models/concerns/seed_entity.rb
+++ app/models/concerns/seed_entity.rb
@@ -23,7 +23,7 @@ module SeedEntity
 
     def visible_fields
       attributes.except('provider', 'uid', 'tokens', 'current_sign_in_at', 'current_sign_in_ip', 'last_sign_in_at',
-                        'last_sign_in_ip', 'sign_in_count', 'encrypted_password').merge({'label' => label})
+                        'last_sign_in_ip', 'sign_in_count', 'encrypted_password').merge({'label' => label, 'id' => id})
     end
 
     def label
